15996ae03e7ffcb8686958524167ef75169372f1
DiogoPCS <diogo.paulino.santos@Hotmail.com>
2026-09-09 12:30:24 -0300

    teste

--- Angular/src/app/home/home.page.ts
+++ Angular/src/app/home/home.page.ts
@@ -14,14 +14,24 @@ export class HomePage {
   produtos: any;
 
   constructor( 
-    public crudService: CrudService
+    public crudService: CrudService,
+    public messageService: MessageService
   ){  }
 
 
   listarProdutos(){
     this.crudService.get('produtos').subscribe(produtos => {
       this.produtos = produtos;
+      this.messageService.show('Todos os produtos foram carregados!');
     });
+  }
+
+  removerProduto(id: any){
+    this.crudService.delete(`produtos/${id}`).subscribe(resp => {
+      console.log(resp);
+      this.messageService.show(`O produto com id = ${id} foi removido!`);
+      this.listarProdutos()
+    })
   }
 
   
