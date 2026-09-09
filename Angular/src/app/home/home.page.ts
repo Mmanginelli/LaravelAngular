8f83b2fe93ed237233ae906a3f6e9cc6c94c6bc7
DiogoPCS <diogo.paulino.santos@Hotmail.com>
2026-09-09 12:08:43 -0300

    teste

--- Angular/src/app/home/home.page.ts
+++ Angular/src/app/home/home.page.ts
@@ -11,8 +11,20 @@ import { CrudService } from '../services/api.service';
 })
 export class HomePage {
 
+  produtos: any;
+
   constructor( 
     public crudService: CrudService
-  ){ }
+  ){  }
+
+
+  listarProdutos(){
+    this.crudService.get('produtos').subscribe(produtos => {
+      this.produtos = produtos;
+    });
+  }
+
+  
+
 
 }
